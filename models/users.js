32282015e4d219364fd39ade9612211d268973a2
Ranto Ny Aina <rantonyandriambahiny@gmail.com>
2025-11-26 10:13:16 +0530

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  login: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  fivondronanaId: {
+    type: Number,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'mp', 'user'],
+    default: 'user'
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
