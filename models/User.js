a14065eabfd7b781d25d1784a2eb587db65844f7
Ranto Ny Aina <rantonyandriambahiny@gmail.com>
2025-11-26 10:45:45 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,17 +4,18 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   login: {
     type: String,
-    required: true,
+    required: [true, 'Login est requis'],
     unique: true,
     trim: true
   },
   password: {
     type: String,
-    required: true
+    required: [true, 'Mot de passe est requis'],
+    minlength: 6
   },
   fivondronanaId: {
     type: Number,
-    required: true
+    required: [true, 'FivondronanaId est requis']
   },
   role: {
     type: String,
@@ -29,16 +30,23 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Hash password before saving
+// Middleware pour hasher le mot de passe avant sauvegarde
 userSchema.pre('save', async function(next) {
+  // Only run this function if password was actually modified
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
+  
+  try {
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-// Compare password method
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+// Méthode pour comparer les mots de passe
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
 };
 
 module.exports = mongoose.model('User', userSchema);
